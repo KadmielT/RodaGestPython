1d96e19df61e156226acfd1993d4dc37b9c801b6
Kadmiel T <132308058+KadmielT@users.noreply.github.com>
2026-02-16 20:52:47 -0300

    Adicionando formulario de endereço ao cliente

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, Endereco
 
-admin.site.register(Cliente)
+class EnderecoInline(admin.StackedInline):
+    model = Endereco
+    can_delete = False
+    extra = 0
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    inlines = [EnderecoInline]
+
+@admin.register(Endereco)
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'cidade', 'uf', 'cep')
+    search_fields = ('cliente__nome', 'cidade', 'bairro', 'cep')
--- clientes/models.py
+++ clientes/models.py
@@ -8,13 +8,13 @@ TIPO_CHOICES = [
     ('PJ', 'Pessoa Jurídica'),
 ]
 
-
 class Cliente(models.Model):
     nome = models.CharField(max_length=150)
     tipo = models.CharField(max_length=2, choices=TIPO_CHOICES)
     documento = models.CharField(max_length=20, unique=True)
     telefone = models.CharField(max_length=20, unique=True)
     email = models.EmailField(blank=True, null=True)
+
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def clean(self):
@@ -71,3 +71,40 @@ class Cliente(models.Model):
 
     def __str__(self):
         return self.nome
+    
+class Endereco (models.Model):
+    cliente = models.OneToOneField(
+        Cliente,
+        on_delete=models.CASCADE,
+        related_name='endereco'
+    )
+    
+    cep = models.CharField(max_length=8)
+    rua = models.CharField(max_length=150)
+    numero = models.CharField(max_length=20)
+    complemento = models.CharField(max_length=150, blank=True, null=True)
+    bairro = models.CharField(max_length=150)
+    cidade = models.CharField(max_length=150)
+    uf = models.CharField(max_length=2)
+
+    data_cadastro = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        super().clean()
+
+        #remove máscara do CEP
+        if self.cep:
+            self.cep = re.sub(r'\D', '', self.cep)
+
+            if len(self.cep) != 8:
+                raise ValidationError({'cep': 'CEP deve conter 8 dígitos.'})
+        
+        #UF deve ser maiúscula e conter 2 caracteres
+        if self.uf:
+            self.uf = self.uf.upper()
+
+            if len(self.uf) != 2:
+                raise ValidationError({'uf': 'UF deve conter 2 caracteres.'})
+            
+        def __str__(self):
+            return f"{self.cliente.nome} - {self.cidade}/{self.uf}"
