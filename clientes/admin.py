f1a0a9a24a2ceddae2384e4b73e0f82efbf0629c
Kadmiel T <132308058+KadmielT@users.noreply.github.com>
2026-02-17 14:29:42 -0300

    Estilização base do sistema

--- clientes/admin.py
+++ clientes/admin.py
@@ -9,8 +9,17 @@ class EnderecoInline(admin.StackedInline):
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
     inlines = [EnderecoInline]
+    # Mostra essas colunas na listagem (tela de "Clientes")
+    list_display = ("nome", "tipo", "documento", "telefone", "email", "data_cadastro")
+
+    # Cria barra de busca (pesquisa por nome, documento, telefone, email)
+    search_fields = ("nome", "documento", "telefone", "email")
+
+    # Define a ordem padrão (mais novo primeiro)
+    ordering = ("-data_cadastro",)
 
 @admin.register(Endereco)
 class EnderecoAdmin(admin.ModelAdmin):
     list_display = ('cliente', 'cidade', 'uf', 'cep')
     search_fields = ('cliente__nome', 'cidade', 'bairro', 'cep')
+
--- rodagest/settings.py
+++ rodagest/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'rodagest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
